8a97de84f1957f91fb9bbbc34af0c59b94a1b534
Ahmedosama101 <123547230+Ahmedosama101@users.noreply.github.com>
2024-06-14 20:59:52 +0300

    BudgetFunctionWorking

--- app/(routes)/dashboard/budgets/_components/CreateBudget.jsx
+++ app/(routes)/dashboard/budgets/_components/CreateBudget.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import EmojiPicker from 'emoji-picker-react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { db } from '@/db';
+import { Budgets } from '@/schema';
+import { useUser } from '@clerk/nextjs';
+import { toast } from 'sonner';
 
 function CreateBudget() {
+  const [emojiIcon, setEmojiIcon]=useState('😂');
+  const [openEmojipicker, setOpenemojipicker]=useState(false);
+  const [name,setName]=useState();
+  const [amount,setAmount]=useState();
+  const {user}=useUser();
+  /* creating Budget Function */
+  const onCreateBudget=async ()=>{
+    const result=await db.insert(Budgets).values({
+      name:name,
+      amount:amount,
+      creactedBy:user?.primaryEmailAddress.emailAddress,
+      icon:emojiIcon
+    }).returning({insertedId:Budgets.id })
+    
+    if (result)
+    {
+      toast('New Budget Created')
+    }
+  }
   return (
     <div>
       <Dialog>
@@ -19,12 +48,37 @@ function CreateBudget() {
             </div></DialogTrigger>
         <DialogContent>
           <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogTitle>Create New Budget</DialogTitle>
             <DialogDescription>
-              This action cannot be undone. This will permanently delete your account
-              and remove your data from our servers.
+             <div className='mt-5'>
+                <Button variant="outline" 
+                className='text-lg'
+                onClick={()=>setOpenemojipicker(!openEmojipicker)}>{emojiIcon }</Button>
+                <div className='absolute'>
+                  <EmojiPicker open={openEmojipicker} onEmojiClick={(e)=>{setEmojiIcon(e.emoji) 
+                    setOpenemojipicker(false)}}/>
+                </div> 
+                <div className='mt-2'>
+                  <h2 className='text-black font-medium my-1'>Budget Name</h2>
+                  <Input placeholder='e.g. Rent'
+                  onChange={(e)=>setName(e.target.value)}/>
+                </div>
+                <div className='mt-2'>
+                  <h2 className='text-black font-medium my-1'>Budget Amount</h2>
+                  <Input placeholder='3000 QAR' type='number'
+                  onChange={(e)=>setAmount(e.target.value)}/>
+                </div>
+               </div>
             </DialogDescription>
           </DialogHeader>
+          <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+          <Button 
+                disabled={!(name&&amount)}
+                onClick={()=>onCreateBudget()}
+                className='mt-5 w-full'>Create Budget</Button>
+          </DialogClose>
+        </DialogFooter>
         </DialogContent>
       </Dialog>
     </div>
--- schema.ts
+++ schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, varchar } from "drizzle-orm/pg-core";
 
 export const Budgets=pgTable("budget", {
     id:serial('id').primaryKey(),
